06cf6a74a0a30c59d618f9c87923e564ec96f1f8
Fachrul Mustofa <fachrulmustofa100@gmail.com>
2022-03-01 15:54:58 +0700

    add validation age >= 8

--- entity/user.go
+++ entity/user.go
@@ -9,12 +9,13 @@ import (
 
 type User struct {
 	GormModel
-	Username string    `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Your username is required"`
-	Email    string    `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Your email is required,email~Invalid email format"`
-	Password string    `gorm:"not null" json:"password" form:"password" valid:"required~Your password is required,minstringlength(6)~Password has to have a minimum length of 6 characters"`
-	Age      uint      `gorm:"not null" json:"age" form:"age" valid:"required~Your age is required"`
-	Photo    []Photo   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"photo"`
-	Comment  []Comment `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comment"`
+	Username    string        `gorm:"not null;uniqueIndex" json:"username" form:"username" valid:"required~Your username is required"`
+	Email       string        `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Your email is required,email~Invalid email format"`
+	Password    string        `gorm:"not null" json:"password" form:"password" valid:"required~Your password is required,minstringlength(6)~Password has to have a minimum length of 6 characters"`
+	Age         uint          `gorm:"not null" json:"age" form:"age" valid:"required~Your age is required,range(8|150)~Age cannot be under 8"`
+	Photo       []Photo       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"photo"`
+	Comment     []Comment     `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"comment"`
+	SocialMedia []SocialMedia `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;" json:"social_media"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
